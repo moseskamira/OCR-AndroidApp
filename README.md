45fec95d6408c2a3da71b413473a221d7b491c9c
moseskamira <moses.african@gmail.com>
2020-02-01 11:50:48 +0300

    Implement OCR in Android App

--- README.md
+++ README.md
@@ -4,12 +4,19 @@ This project involves an implementation of the Optical Character Recognizer conc
 ## Technology/Language
 - Android/Java
 
-- Graph Library/lecho:hellocharts-library for Line Graphs
-
-- PhilJay:MPAndroidChart Library for Bar Graphs
+- Vision Library (com.google.android.gms:play-services-vision:19.0.0)
 
 ## Project Development Objectives
 - To test the implementation of OCR (Optical Character Recognizer) in Android Applications
+
+## How To Test App
+- clone the repo at the Link: https://github.com/moseskamira/OCR-AndroidApp.git
+
+- Run the project
+
+- Click CAPTURE IMAGE button to capture the picture containing characters from which the Text shall be extracted.
+
+- Click GENERATE TEXT button and observer the display.
 
 ## Screenshot
 ### Captured Image With Generated Text
--- app/src/main/java/com/example/ocrandroidapp/MainActivity.java
+++ app/src/main/java/com/example/ocrandroidapp/MainActivity.java
@@ -2,24 +2,19 @@ package com.example.ocrandroidapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +22,6 @@ import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     ImageView ocrImageView;
@@ -35,8 +29,6 @@ public class MainActivity extends AppCompatActivity {
     Button generateTextButton;
     TextView generatedTextView;
     protected String imagePath;
-    protected boolean imageTaken;
-    protected static final String PHOTO_TAKEN = "photo_taken";
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
     private static final int CAMERA_REQUEST = 1888;
     Bitmap capturedPhoto;
@@ -62,17 +54,8 @@ public class MainActivity extends AppCompatActivity {
                     Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(cameraIntent, CAMERA_REQUEST);
                 }
-
             }
         });
-
-//        generateTextButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                generateText();
-//
-//            }
-//        });
 
     }
 
@@ -82,16 +65,13 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
     {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION_CODE)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
-            else
-            {
+            else {
                 Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
             }
         }
@@ -100,11 +80,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
-        {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
             capturedPhoto = (Bitmap) data.getExtras().get("data");
             setImageToView();
-
         }
     }
 
@@ -113,16 +91,13 @@ public class MainActivity extends AppCompatActivity {
             ocrImageView.setImageBitmap(capturedPhoto);
             captureImageButton.setVisibility(View.GONE);
             generateTextButton.setVisibility(View.VISIBLE);
-
             generateTextButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Log.d("HERE", "GENETEXT");
                  generateText();
                 }
             });
             generatedTextView.setVisibility(View.VISIBLE);
-
         }
     }
 
@@ -133,19 +108,13 @@ public class MainActivity extends AppCompatActivity {
 
         }else {
             if (capturedPhoto !=null) {
-                Log.d("NOW", "HERE");
                 Frame frame = new Frame.Builder().setBitmap(capturedPhoto).build();
                 SparseArray<TextBlock> items = textRecognizer.detect(frame);
                 StringBuilder stringBuilder = new StringBuilder();
-                Log.d("ITEMSLIST", items.toString());
-                Log.d("BLOCKBUILDER", stringBuilder.toString());
-
                 for (int i=0; i<items.size(); i++) {
                     TextBlock myItem = items.valueAt(i);
-                    Log.d("ITEM", items.toString());
                     stringBuilder.append(myItem.getValue());
                     stringBuilder.append("\n");
-
                 }
                 generateTextButton.setVisibility(View.GONE);
                 generatedTextView.setVisibility(View.VISIBLE);
@@ -153,16 +122,8 @@ public class MainActivity extends AppCompatActivity {
 
             }else {
                 Toast.makeText(this, "capturedPhoto is null", Toast.LENGTH_LONG).show();
-
             }
-
-
         }
-
     }
-
-
-
-
 
 }
